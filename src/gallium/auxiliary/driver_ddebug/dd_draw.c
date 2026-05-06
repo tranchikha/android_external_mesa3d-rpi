bminor/mesa-mesa

commit 6fe6c18c12ed7120418dc9b379198895842e4d7b

util: add/use new os_mkdir() function
os_mkdir() is a simple wrapper around mkdir() or _mkdir().

Remove a bunch of unneeded #includes in dd_util.h.  Testing by
compiling llvmpipe and radeonsi (the only user of the dd_util.h
header).

There's a few other mkdir() callsites that I haven't touched but
could be updated to os_mkdir().

Signed-off-by: Brian Paul <brian.paul@broadcom.com>
Tested-by: Mauro Rossi <issor.oruam@gmail.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/35841>

src/gallium/auxiliary/driver_ddebug/dd_draw.c
@@ -27,6 +27,7 @@
 
 #include "dd_pipe.h"
 
+#include "util/os_file.h"
 #include "util/u_dump.h"
 #include "util/format/u_format.h"
 #include "util/u_framebuffer.h"
@@ -54,7 +55,7 @@ dd_get_debug_filename_and_mkdir(char *buf, size_t buflen, bool verbose)
 
    snprintf(dir, sizeof(dir), "%s/"DD_DIR, debug_get_option("HOME", "."));
 
-   if (mkdir(dir, 0774) && errno != EEXIST)
+   if (os_mkdir(dir, 0774) && errno != EEXIST)
       fprintf(stderr, "dd: can't create a directory (%i)\n", errno);
 
    snprintf(buf, buflen, "%s/%s_%u_%08u", dir, proc_name, (unsigned int)getpid(),

src/gallium/auxiliary/driver_ddebug/dd_util.h
@@ -29,22 +29,6 @@
 #define DD_UTIL_H
 
 #include <stdio.h>
-#include <errno.h>
-
-#include "c99_alloca.h"
-#include "util/u_atomic.h"
-#include "util/u_debug.h"
-#include "util/u_string.h"
-
-#include "util/detect.h"
-#if DETECT_OS_POSIX
-#include <unistd.h>
-#include <sys/stat.h>
-#elif DETECT_OS_WINDOWS
-#include <direct.h>
-#include <process.h>
-#define mkdir(dir, mode) _mkdir(dir)
-#endif
 
 struct pipe_screen;
 

src/gallium/auxiliary/gallivm/lp_bld_debug.cpp
@@ -46,8 +46,8 @@
 #include "util/detect_os.h"
 #include "util/u_math.h"
 #include "util/u_debug.h"
+#include "util/os_file.h"
 
-#include "driver_ddebug/dd_util.h"
 #include "lp_bld_debug.h"
 #include "lp_bld_intr.h"
 
@@ -377,7 +377,7 @@ lp_function_add_debug_info(gallivm_state *gallivm, LLVMValueRef func, LLVMTypeRe
    if (!gallivm->file) {
       uint32_t shader_index = p_atomic_add_return(&global_shader_index, 1);
 
-      mkdir(LP_NIR_SHADER_DUMP_DIR, 0755);
+      os_mkdir(LP_NIR_SHADER_DUMP_DIR, 0755);
 
       asprintf(&gallivm->file_name, "%s/%u.nir", LP_NIR_SHADER_DUMP_DIR, shader_index);
 

src/util/os_file.c
@@ -5,6 +5,7 @@
 
 #include "os_file.h"
 #include "detect_os.h"
+#include "util/detect.h"
 
 #include <errno.h>
 #include <fcntl.h>
@@ -12,6 +13,7 @@
 #include <sys/stat.h>
 
 #if DETECT_OS_WINDOWS
+#include <direct.h>
 #include <io.h>
 #define open _open
 #define fdopen _fdopen
@@ -21,6 +23,7 @@
 #define O_CREAT _O_CREAT
 #define O_EXCL _O_EXCL
 #define O_WRONLY _O_WRONLY
+#define mkdir(dir, mode) _mkdir(dir)
 #else
 #include <unistd.h>
 #ifndef F_DUPFD_CLOEXEC
@@ -266,5 +269,16 @@ os_same_file_description(int fd1, int fd2)
 #else
    /* Otherwise we can't tell */
    return -1;
+#endif
+}
+
+
+int
+os_mkdir(const char *pathname, int mode)
+{
+#if DETECT_OS_WINDOWS
+   return _mkdir(pathname);
+#else
+   return mkdir(pathname, mode);
 #endif
 }

src/util/os_file.h
@@ -52,6 +52,15 @@ os_read_file(const char *filename, size_t *size);
 int
 os_same_file_description(int fd1, int fd2);
 
+/*
+ * Make a directory.  The file permissions bitmask 'mode' may be ignored
+ * (such as on Windows).
+ * Return 0 for success, -1 on failure (setting errno).
+*/
+int
+os_mkdir(const char *pathname, int mode);
+
+
 #ifdef __cplusplus
 }
 #endif

tranchikha/android_external_mesa3d-rpi

commit 2b9e491b6789f60a7993cc9b74fe5ac7fa60c9c5

ddebug: Fix use of alloca() without #include "c99_alloca.h"
Fixes: 6fe6c18c12ed ("util: add/use new os_mkdir() function")
Signed-off-by: Jon Turney <jon.turney@dronecode.org.uk>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41389>

src/gallium/auxiliary/driver_ddebug/dd_draw.c
@@ -40,6 +40,7 @@
 #include "util/os_time.h"
 #include <inttypes.h>
 #include "util/detect.h"
+#include "c99_alloca.h"
 
 void
 dd_get_debug_filename_and_mkdir(char *buf, size_t buflen, bool verbose)